a6553b9ec68e8a14fc6130ce31815d92f7a6698d
jefb <jeffrey_bouwhuis123@msn.com>
2025-06-18 17:20:31 +0200

    ok

--- backend/internal/data/ent/schema/attachment.go
+++ backend/internal/data/ent/schema/attachment.go
@@ -3,7 +3,6 @@ package schema
 import (
 	"entgo.io/ent"
 	"entgo.io/ent/schema/field"
-	"entgo.io/ent/schema/index"
 	"github.com/google/uuid"
 	"github.com/sysadminsmedia/homebox/backend/internal/data/ent/schema/mixins"
 )
@@ -26,35 +25,15 @@ func (Attachment) Fields() []ent.Field {
 		field.Bool("primary").Default(false),
 		field.String("title").Default(""),
 		field.String("path").Default(""),
-<<<<<<< HEAD
-		field.String("related_type").NotEmpty(),
-		field.Int("related_id").Positive(),
-=======
 		// NIEUWE POLYMORPHIC VELDEN
 		field.String("related_type").
 			Comment("Type of related entity: items, maintenance_entries, incidents, reports"),
 		field.UUID("related_id", uuid.UUID{}).
 			Comment("ID of the related entity"),
->>>>>>> Attachments
 	}
 }
 
 // Edges of the Attachment.
 func (Attachment) Edges() []ent.Edge {
-<<<<<<< HEAD
 	return nil
-=======
-	return []ent.Edge{
-		// Verwijder de oude directe item edge
-		// We gebruiken nu related_type + related_id voor polymorphic relations
-	}
-}
-
-// Indexes of the Attachment.
-func (Attachment) Indexes() []ent.Index {
-	return []ent.Index{
-		index.Fields("related_type", "related_id"),
-		index.Fields("related_type"),
-	}
->>>>>>> Attachments
 }
